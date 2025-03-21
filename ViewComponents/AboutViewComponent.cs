f7bf7bb831fd03e5ad90b5a9d764dbf170281480
Anhphap2004 <phapmeo2k4@gmail.com>
2025-03-21 09:30:03 +0700

    hiển thị about trong trang chủ

---
+++ ViewComponents/AboutViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using PhapClinicX.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace PhapClinicX.ViewComponents
+{
+   
+    public class AboutViewComponent : ViewComponent
+    {
+        private readonly ClinicManagementContext _context;
+        public AboutViewComponent(ClinicManagementContext context)
+        { 
+            
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var about = await _context.Abouts.ToListAsync();
+            return View(about);
+        }
+    }
+}
