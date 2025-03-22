7935a390496f186624b22aa89e53c0fa0936eb7d
Anhphap2004 <phapmeo2k4@gmail.com>
2025-03-22 14:48:28 +0700

    hiển thị trang blog

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhapClinicX.Models;
+using Microsoft.EntityFrameworkCore;
+namespace PhapClinicX.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly ClinicManagementContext _context;
+        public BlogController(ClinicManagementContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.blogcategories = _context.BlogCategories.Where(p => p.IsActive == true).ToList();
+            return View();
+        }
+    }
+}
