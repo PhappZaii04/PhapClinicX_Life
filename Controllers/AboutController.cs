040e8f5c1f1f22acb916a8ac51ca31067d564b07
Anhphap2004 <phapmeo2k4@gmail.com>
2025-03-22 10:40:03 +0700

    trang riêng about hoàn thành

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PhapClinicX.Models;
+
+namespace PhapClinicX.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly ClinicManagementContext _context;
+        public AboutController(ClinicManagementContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.Faqs = _context.Faqs.ToList();
+            return View();
+        }
+    }
+}
