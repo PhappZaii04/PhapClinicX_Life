390886af575b8ba7d46998d796119319e5a23781
Anhphap2004 <phapmeo2k4@gmail.com>
2025-03-22 18:30:19 +0700

    giao diện tổng đặt lịch khám

---
+++ Controllers/BookingController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PhapClinicX.Models;
+namespace PhapClinicX.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly ClinicManagementContext _context;
+        public BookingController(ClinicManagementContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
