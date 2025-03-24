fe05e7f3474fb0923fccd633e63618762d8ee776
Anhphap2004 <phapmeo2k4@gmail.com>
2025-03-24 21:58:37 +0700

    Ghép Template Admin

--- Program.cs
+++ Program.cs
@@ -35,6 +35,11 @@ app.UseAuthorization();
 app.UseSession();
 app.MapStaticAssets();
 
+
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
