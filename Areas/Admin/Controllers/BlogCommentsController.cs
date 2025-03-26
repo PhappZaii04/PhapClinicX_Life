d4c9c3e4b2f461b9f2cd880a30024f8082701a8b
Anhphap2004 <phapmeo2k4@gmail.com>
2025-03-26 20:46:38 +0700

    Quản lý bình luận bài viết

---
+++ Areas/Admin/Controllers/BlogCommentsController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PhapClinicX.Models;
+
+namespace PhapClinicX.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BlogCommentsController : Controller
+    {
+        private readonly ClinicManagementContext _context;
+
+        public BlogCommentsController(ClinicManagementContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/BlogComments
+        public async Task<IActionResult> Index()
+        {
+            var clinicManagementContext = _context.BlogComments.Include(b => b.Blog).Include(b => b.User);
+            
+            return View(await clinicManagementContext.ToListAsync());
+        }
+
+        // GET: Admin/BlogComments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blogComment = await _context.BlogComments
+                .Include(b => b.Blog)
+                .Include(b => b.User)
+                .FirstOrDefaultAsync(m => m.CommentId == id);
+            if (blogComment == null)
+            {
+                return NotFound();
+            }
+
+            return View(blogComment);
+        }
+
+        // GET: Admin/BlogComments/Create
+        public IActionResult Create()
+        {
+            ViewData["BlogId"] = new SelectList(_context.Blogs, "BlogId", "BlogId");
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId");
+            return View();
+        }
+
+        // POST: Admin/BlogComments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CommentId,BlogId,UserId,Comment,CreatedAt")] BlogComment blogComment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(blogComment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BlogId"] = new SelectList(_context.Blogs, "BlogId", "BlogId", blogComment.BlogId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", blogComment.UserId);
+            return View(blogComment);
+        }
+
+        // GET: Admin/BlogComments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blogComment = await _context.BlogComments.FindAsync(id);
+            if (blogComment == null)
+            {
+                return NotFound();
+            }
+            ViewData["BlogId"] = new SelectList(_context.Blogs, "BlogId", "BlogId", blogComment.BlogId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", blogComment.UserId);
+            return View(blogComment);
+        }
+
+        // POST: Admin/BlogComments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CommentId,BlogId,UserId,Comment,CreatedAt")] BlogComment blogComment)
+        {
+            if (id != blogComment.CommentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(blogComment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BlogCommentExists(blogComment.CommentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BlogId"] = new SelectList(_context.Blogs, "BlogId", "BlogId", blogComment.BlogId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", blogComment.UserId);
+            return View(blogComment);
+        }
+
+        // GET: Admin/BlogComments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blogComment = await _context.BlogComments
+                .Include(b => b.Blog)
+                .Include(b => b.User)
+                .FirstOrDefaultAsync(m => m.CommentId == id);
+            if (blogComment == null)
+            {
+                return NotFound();
+            }
+
+            return View(blogComment);
+        }
+
+        // POST: Admin/BlogComments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var blogComment = await _context.BlogComments.FindAsync(id);
+            if (blogComment != null)
+            {
+                _context.BlogComments.Remove(blogComment);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BlogCommentExists(int id)
+        {
+            return _context.BlogComments.Any(e => e.CommentId == id);
+        }
+    }
+}
