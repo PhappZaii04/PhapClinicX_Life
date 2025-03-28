2412830acf3a9e6b470f8805efe38ab40d599ba7
Anhphap2004 <phapmeo2k4@gmail.com>
2025-03-29 00:25:41 +0700

    Update sản phẩm

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -16,8 +16,36 @@ namespace PhapClinicX.Controllers
         }
         public IActionResult Index()
         {
-            var Products = _context.Products.Where(p=>p.IsActive == true).ToList();
+            var Products = _context.Products.Where(p => p.IsActive == true).ToList();
             return View(Products);
+        }
+
+        [Route("/san-pham/{alias}-{id}.html")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .Include(p => p.Category)
+                .Where(p => p.IsActive)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.Category = await _context.ProductCategories
+                .Where(p => p.IsActive == true && p.CategoryId == product.CategoryId)
+                .ToListAsync(); // Truy vấn bất đồng bộ tốt hơn
+
+            ViewBag.RelatedProducts = await _context.Products
+                .Where(p => p.IsActive && p.CategoryId == product.CategoryId && p.ProductId != product.ProductId)
+                .ToListAsync(); // Thêm điều kiện tránh lặp chính nó
+            return View(product);
         }
     }
 }
